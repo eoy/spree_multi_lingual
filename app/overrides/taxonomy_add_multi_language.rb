f7d6b38eb2aada3f53207b949585cc55e08168f6
Joakim Runeberg <joakim.runeberg@gmail.com>
2013-09-19 09:35:07 +0300

    Update taxonomy_add_multi_language.rb

--- app/overrides/taxonomy_add_multi_language.rb
+++ app/overrides/taxonomy_add_multi_language.rb
@@ -1,11 +1,11 @@
-Deface::Override.new(
-        :virtual_path  => "spree/admin/taxonomies/edit",
-        :replace => "code[erb-loud]:contains('t(:taxonomy_edit)')",
-        :name    => "taxonomies_add_multi_language",
-        :partial => "spree/admin/taxonomies/multi_language_links")
-
-Deface::Override.new(
-        :virtual_path  => "spree/admin/taxons/edit",
-        :insert_before => '[data-hook="buttons"]',
-        :text => '<%= render "spree/admin/taxons/multi_scripts", :object => @taxon -%>',
-        :name    => "taxon_add_language_dropdown")
+#Deface::Override.new(
+#        :virtual_path  => "spree/admin/taxonomies/edit",
+#        :replace => "code[erb-loud]:contains('t(:taxonomy_edit)')",
+#        :name    => "taxonomies_add_multi_language",
+#        :partial => "spree/admin/taxonomies/multi_language_links")
+#
+#Deface::Override.new(
+#        :virtual_path  => "spree/admin/taxons/edit",
+#        :insert_before => '[data-hook="buttons"]',
+#        :text => '<%= render "spree/admin/taxons/multi_scripts", :object => @taxon -%>',
+#        :name    => "taxon_add_language_dropdown")
