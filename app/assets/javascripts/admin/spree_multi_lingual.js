6cca06b4d9eae5ff773eab4dab70596f9aeaf3fd
Joakim Runeberg <joakim.runeberg@gmail.com>
2013-10-17 15:40:12 +0300

    epic taxon edit fix

--- app/assets/javascripts/admin/spree_multi_lingual.js
+++ app/assets/javascripts/admin/spree_multi_lingual.js
@@ -1,16 +1,15 @@
 //= require admin/spree_backend
 //= require admin/spree_multi_lingual_class
-
 var handle_rename = function(e, data) {
   last_rollback = data.rlbk;
   var node = data.rslt.obj;
   var name = data.rslt.new_name;
-  var params = {_method: "put", authenticity_token: AUTH_TOKEN};
+  var params = {_method: "put", "taxon[name]": name, authenticity_token: AUTH_TOKEN};
   params[param_name] = name;
   $.ajax({
     type: "POST",
     dataType: "json",
-    url: base_url + node.attr("id"),
+    url: "taxons" + '/' + node.attr("id"),
     data: params,
     error: handle_ajax_error
   });
