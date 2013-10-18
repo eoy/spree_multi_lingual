4915d3ff9b39e05eb524e15d1e7144ab1ae9afd0
Joakim Runeberg <joakim.runeberg@gmail.com>
2013-10-18 10:59:13 +0300

    mailer localization

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -3,15 +3,20 @@ Spree::OrderMailer.class_eval do
   def confirm_email(order, resend = false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     I18n.locale = @order.locale
+    @name = @order.billing_address.firstname
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
+    subject += "#{Spree.t('order_mailer.confirm_email.subject')} (##{@order.number})"
     mail(to: @order.email, from: from_address, subject: subject)
+
+    finished("deliverability_message")
   end
 
   def cancel_email(order, resend = false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+    I18n.locale = @order.locale
+    @name = @order.billing_address.firstname
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
+    subject += "#{Spree.t('order_mailer.cancel_email.subject')} (##{@order.number})"
     mail(to: @order.email, from: from_address, subject: subject)
   end
 end
